cc3f9f3a4bf1d1746952694e8a7868a666cb8fa8
.newmind <git.newmind@newmind.pw>
2026-05-09 13:52:32 +0200

    add debug output to requests by DEBUG_HTTP

--- test/TestHTTP.hs
+++ test/TestHTTP.hs
@@ -15,6 +15,8 @@ import Autodocodec (toJSONViaCodec, eitherDecodeJSONViaCodec, HasCodec)
 import Network.HTTP.Simple
 import Network.HTTP.Client.Conduit (responseTimeoutMicro)
 import qualified Data.CaseInsensitive as CI
+import System.Environment (lookupEnv)
+import System.IO (hPutStrLn, stdout)
 
 -- Generic HTTP POST call that encodes request and decodes response via autodocodec
 -- Returns (status code, decoded response)
@@ -24,17 +26,31 @@ httpCall :: (HasCodec req, HasCodec resp)
          -> req
          -> IO (Int, resp)
 httpCall endpoint headers request = do
+  debug <- lookupEnv "DEBUG_HTTP"
   req <- parseRequest $ "POST " ++ endpoint
 
-  let headerList = [(CI.mk $ TE.encodeUtf8 k, TE.encodeUtf8 v) | (k, v) <- headers]
+  let body = Aeson.encode $ toJSONViaCodec request
+      headerList = [(CI.mk $ TE.encodeUtf8 k, TE.encodeUtf8 v) | (k, v) <- headers]
   let req' = setRequestHeaders headerList
-           $ setRequestBodyLBS (Aeson.encode $ toJSONViaCodec request)
+           $ setRequestBodyLBS body
            $ setRequestResponseTimeout (responseTimeoutMicro 300000000)  -- 5 minutes timeout
            $ req
+
+  case debug of
+    Just _ -> do
+      hPutStrLn stdout $ "[DEBUG_HTTP] --> POST " ++ endpoint
+      BSL.hPut stdout body >> hPutStrLn stdout ""
+    Nothing -> return ()
 
   response <- httpLBS req'
   let responseBody = getResponseBody response
       statusCode = getResponseStatusCode response
+
+  case debug of
+    Just _ -> do
+      hPutStrLn stdout $ "[DEBUG_HTTP] <-- " ++ show statusCode
+      BSL.hPut stdout responseBody >> hPutStrLn stdout ""
+    Nothing -> return ()
   case eitherDecodeJSONViaCodec responseBody of
     Right resp -> return (statusCode, resp)
     Left err -> error $ "Failed to parse response: " ++ err ++ "\nResponse body: " ++ show responseBody
